bb17c3c1aca7a706d5a60dd7e27038ce8bc2e933
tetianaklius <tetyanaklyus@gmail.com>
2023-07-08 00:26:05 +0300

    hw_2_1

---
+++ src/components/Posts/Post/Post.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import PostDetail from "../PostDetail/PostDetail";
+
+const Post = ({post}) => {
+    const [isShow, setIsShow] = useState(false);
+    const {id, title} = post;
+    return (
+        <div>
+            <div>id: {id}</div>
+            <div>{title}</div>
+            <button onClick={()=>setIsShow(prev=>!prev)}>{isShow?"Hide":"Show"}</button>
+            {
+                isShow&& <PostDetail post={post}/>
+            }
+        </div>
+    );
+};
+
+export default Post;
