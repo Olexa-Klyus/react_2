88e722efbd9f40d14039be32ad6477fa4cc0062c
tetianaklius <tetyanaklyus@gmail.com>
2023-07-20 16:24:49 +0300

    hw_5_2

---
+++ src/Routing/router.js
@@ -0,0 +1,10 @@
+import {createBrowserRouter} from "react-router-dom";
+
+export const router = createBrowserRouter([
+    // стандартна структура:
+    {
+        path: "/",
+        element: <div>Hello world!</div>,
+    },
+    // тепер робимо перший елемент,
+]);
